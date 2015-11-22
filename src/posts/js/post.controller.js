ac5bf5d50ac0213497d65ffd44264b5a860c2075
Anis BOUHACHEM <anis.bouhachem@tritux.com>
2015-11-22 21:57:19 +0100

    Indent code after changing editor to netbeans.

--- src/posts/js/post.controller.js
+++ src/posts/js/post.controller.js
@@ -1,16 +1,16 @@
 /*global angular*/
 angular.module('post')
 	/**
-    * @ngdoc controller
-    * @name post.controller:PostMainController
-    *
-    * @description
-    * # main post controller
-    *
-    * The main post controller
-    *
-    */
-    .controller('PostMainController', ["PostManager", function (PostManager) {
+	* @ngdoc controller
+	* @name post.controller:PostMainController
+	*
+	* @description
+	* # main post controller
+	*
+	* The main post controller
+	*
+	*/
+	.controller('PostMainController', ["PostManager", function (PostManager) {
 		"use strict";
 		var scope = this;
 		scope.text = "Hello world !";

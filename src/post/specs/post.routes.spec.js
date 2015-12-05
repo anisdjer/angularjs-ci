5f98f00b3d3ac50fe0637c3e142c90de49f9b337
Anis BOUHACHEM <anis.bouhachem@tritux.com>
2015-12-05 19:59:23 +0100

    #1 app routes test

--- src/post/specs/post.routes.spec.js
+++ src/post/specs/post.routes.spec.js
@@ -4,18 +4,24 @@ describe('Post module routes', function () {
     "use strict";
     beforeEach(module('post'));
 
-    var $state, $rootScope;
+    var $state, $rootScope, $location;
 
     beforeEach(inject(function ($injector) {
         $rootScope = $injector.get("$rootScope");
+        $location = $injector.get("$location");
         $state = $injector.get("$state");
     }));
 
     afterEach(function() {});
 
     describe("main route", function () {
-        it('should be called', function () {
+        it('should exist', function () {
             expect($state.href("post")).toEqual('#/posts');
+        });
+        it('should be called', function () {
+            $location.path('/posts');
+            $rootScope.$digest();
+            expect($state.current.name).toEqual("post");
         });
     });
 
